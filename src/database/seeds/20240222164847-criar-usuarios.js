a6b64c534dd694c736b8ec1fb914500652d7ba59
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-02-22 14:02:42 -0300

    criado exemplo de seeds

---
+++ src/database/seeds/20240222164847-criar-usuarios.js
@@ -0,0 +1,37 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'John Doe 1',
+          email: 'JohnDoe1@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe 2',
+          email: 'JohnDoe22@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'John Doe 3',
+          email: 'JohnDoe3@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
