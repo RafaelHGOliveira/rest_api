e711a9fe5a284c74c6736ee15d784508d77730c5
Rafael Henrique Gonçalves de Oliveira <rafael.h.g.oliveira@gmail.com>
2024-02-27 15:12:36 -0300

    aceitando apenas upload de imagens

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,8 +1,23 @@
-class HomeController {
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
+class FotoController {
   async store(req, res) {
-    const { originalname, filename } = req.file;
-    res.json({ originalname, filename });
+    upload(req, res, (err) => {
+      if (err) {
+        // Se houver um erro do Multer
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      // Se não houver erro, continua o fluxo
+      const { originalname, filename } = req.file;
+      return res.json({ originalname, filename });
+    });
   }
 }
 
-export default new HomeController();
+export default new FotoController();
